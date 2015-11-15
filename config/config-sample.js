0519e5745f949114595a6edd6cc5e4ca5f0c3dd6
megatricycle <megatricycle@gmail.com>
2015-11-15 18:13:37 +0800

    placed database settings in a config file

---
+++ config/config-sample.js
@@ -0,0 +1,9 @@
+// since magkakaiba tayo ng database settings, eto yung local settings natin na pagcoconnectan ng web app.
+
+// rename or duplicate this file named 'config.js' and adjust your settings accordingly. nasa gitignore to so hindi tayo magkakaapektuhan ng settings
+
+module.exports = {
+  'username': 'username',
+  'password': 'password',
+  'database': 'database'
+};
